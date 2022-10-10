413b2334377cdb498d23b5cdef11657002f33382
AleksandraBoch <shemaFoxy@gmail.com>
2022-10-10 20:16:28 +0300

    Updates

---
+++ precache-manifest.0ca6cee583beb7a5e9fa9bc93ff85987.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3eeab88a1d4f29e7e785bc47e208d250",
+    "url": "/ToDoList/index.html"
+  },
+  {
+    "revision": "d4a3a00a8d6c7f2a587e",
+    "url": "/ToDoList/static/css/main.8b8b6b6c.chunk.css"
+  },
+  {
+    "revision": "1b07c4b7380da76b66b6",
+    "url": "/ToDoList/static/js/2.093bf16b.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/ToDoList/static/js/2.093bf16b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d4a3a00a8d6c7f2a587e",
+    "url": "/ToDoList/static/js/main.091ae03a.chunk.js"
+  },
+  {
+    "revision": "86c4286c12cc5c044c93",
+    "url": "/ToDoList/static/js/runtime-main.1aff77a7.js"
+  }
+]);
